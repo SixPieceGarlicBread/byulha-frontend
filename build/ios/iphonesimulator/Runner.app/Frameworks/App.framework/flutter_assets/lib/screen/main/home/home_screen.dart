2ef3ec0f24ae4bde81f9da928f9eee4050e03a62
leonaxdo <32193248@dankook.ac.kr>
2023-12-05 14:33:41 +0900

    list 구현

--- build/ios/iphonesimulator/Runner.app/Frameworks/App.framework/flutter_assets/lib/screen/main/home/home_screen.dart
+++ build/ios/iphonesimulator/Runner.app/Frameworks/App.framework/flutter_assets/lib/screen/main/home/home_screen.dart
@@ -25,75 +25,62 @@ class HomeScreen extends ConsumerWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 200, // 광고 배너의 높이
-            child: PageView.builder(
-              controller: _pageController,
-              itemCount: _totalAds,
-              itemBuilder: (_, index) {
-                return Container(
-                  color: Colors.grey, // 광고 이미지를 넣을 수 있음
-                  child: Center(
-                    child: Text('배너 ${index + 1}'),
-                  ),
-                );
-              },
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 200, // 광고 배너의 높이
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: _totalAds,
+                itemBuilder: (_, index) {
+                  return Container(
+                    color: Colors.grey, // 광고 이미지를 넣을 수 있음
+                    child: Center(
+                      child: Text('배너 ${index + 1}'),
+                    ),
+                  );
+                },
+              ),
             ),
           ),
-          SizedBox(height: 10), // 페이지 인디케이터와 광고 배너 사이의 간격
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(
-              _totalAds,
-                  (index) => Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                child: CircleAvatar(
-                  radius: 5,
-                  backgroundColor: ref.watch(currentPageProvider) == index
-                      ? Colors.blue
-                      : Colors.grey,
+          // 기타 SliverToBoxAdapter 위젯들...
+          SliverToBoxAdapter(
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ImageRecScreen()),
+                  );
+                },
+                child: Text('이미지로 향수 추천받기'),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 120, vertical: 40),
                 ),
               ),
             ),
           ),
-          SizedBox(height: 20), // 버튼 위의 간격
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                // image_rec.dart 페이지로 이동
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ImageRecScreen()),
-                );
-              },
-              child: Text('이미지로 향수 추천받기'),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 120, vertical: 40),
-              ),
-            ),
-          ),
-          SizedBox(height: 20), // 버튼 아래의 간격
-          Padding(
+          SliverPadding(
             padding: const EdgeInsets.all(16.0),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text('Best Rated'),
+            sliver: SliverToBoxAdapter(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Best Rated'),
+              ),
             ),
           ),
-          Expanded(
-            child: perfumeList.when(
-              data: (data) => GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 3 / 4,
-                ),
-                itemCount: data.content.length,
-                itemBuilder: (context, index) {
+          perfumeList.when(
+            data: (data) => SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 4,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                    (context, index) {
                   final perfume = data.content[index];
                   return Card(
-                    color: Colors.white, // 카드의 배경색을 흰색으로 설정
+                    color: Colors.white,
                     clipBehavior: Clip.antiAliasWithSaveLayer,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8),
@@ -107,7 +94,7 @@ class HomeScreen extends ConsumerWidget {
                             perfume.thumbnailUrl!,
                             fit: BoxFit.cover,
                           )
-                              : const Placeholder(), // 썸네일이 없을 경우 대체 이미지
+                              : const Placeholder(),
                         ),
                         Padding(
                           padding: const EdgeInsets.all(8.0),
@@ -123,13 +110,25 @@ class HomeScreen extends ConsumerWidget {
                     ),
                   );
                 },
+                childCount: data.content.length,
               ),
-              loading: () => const CircularProgressIndicator(),
-              error: (e, stack) => Text('An error occurred: $e'),
+            ),
+            loading: () => SliverFillRemaining(
+              child: Center(child: CircularProgressIndicator()),
+            ),
+            error: (e, stack) => SliverFillRemaining(
+              child: Center(child: Text('An error occurred: $e')),
             ),
           ),
-         
-
+          SliverToBoxAdapter(
+            child: OutlinedButton(
+              onPressed: () {
+                final currentPage = ref.read(currentPageProvider);
+                ref.read(perfumeListProvider.notifier).getPerfumeList(currentPage + 1, 10);
+              },
+              child: const Text('더보기'),
+            ),
+          ),
         ],
       ),
     );
